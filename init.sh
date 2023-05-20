c9ca9a32abe0ca9bd43a24a40251ca6c89d07dc0
VPKSoft <vpksoft@vpksoft.net>
2023-05-20 13:36:02 +0300

    Fix macOS script?

--- init.sh
+++ init.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
+
+# Based on a code by Krystian Safjan: See: https://safjan.com/bash-determine-if-linux-or-macos/
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
 npm install
 mkdir ./dist
 cargo install --path ./src-tauri
-wget https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-web.zip
+# wget https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-web.zip
+curl https://use.fontawesome.com/releases/v6.4.0/fontawesome-free-6.4.0-web.zip -o ./fontawesome-free-6.4.0-web.zip
+
+if [ "$machine" == "Mac" ]; then # unzip doesn't seem to create directories in macOs
+    mkdir -p ./src/assets/css
+
 unzip fontawesome-free-6.4.0-web.zip -d ./src/assets/css
 rm ./fontawesome-free-6.4.0-web.zip
