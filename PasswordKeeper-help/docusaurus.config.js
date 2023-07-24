9f3bcac1f242de017a6628a2efeaf540a7463334
VPKSoft <vpksoft@vpksoft.net>
2023-07-24 18:34:48 +0300

    Adjust Docusaurus config.

--- PasswordKeeper-help/docusaurus.config.js
+++ PasswordKeeper-help/docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
-    url: "https://your-docusaurus-test-site.com",
+    url: "https://vpksoft.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/PasswordKeeper/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "VPKSoft", // Usually your GitHub org/user name.
     projectName: "PasswordKeeper", // Usually your repo name.
+    trailingSlash: false,
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
