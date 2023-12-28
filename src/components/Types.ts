223e917e602d7cf16b1450c32e7d5b59f0b4acc6
VPKSoft <vpksoft@vpksoft.net>
2023-12-28 17:57:29 +0200

    Fix the TOTP key generator component.

--- src/components/Types.ts
+++ src/components/Types.ts
@@ -16,6 +16,8 @@ type Auth2Fa = {
     key: string;
     /** A value indicating whether the key was successfully generated. */
     success: boolean;
+    /** An error message if an error occurred in key generation. */
+    error_message: string;
 };
 
 export type { CommonProps, Auth2Fa };
