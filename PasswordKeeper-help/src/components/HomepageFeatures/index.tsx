c7d7b9eef33c7dc0c4b9078ef89137ee0289dd96
VPKSoft <vpksoft@vpksoft.net>
2024-11-30 14:48:55 +0200

    This file wasn't supposed to be changed.

--- PasswordKeeper-help/src/components/HomepageFeatures/index.tsx
+++ PasswordKeeper-help/src/components/HomepageFeatures/index.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React from "react";
 import clsx from "clsx";
 import Translate, { translate } from "@docusaurus/Translate";
 import Link from "@docusaurus/Link";
